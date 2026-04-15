f49991d22defec0de6dffa944a791158a7ebeedf
damm <damm.deolarte@gmail.com>
2026-04-15 16:53:00 -0600

    Añadir restricción de unicidad al campo 'name' en el modelo ItemModel

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class ItemModel(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(120), nullable=False)
+    name = Column(String(120), nullable=False, unique=True)
     price = Column(Float, nullable=False)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now)
